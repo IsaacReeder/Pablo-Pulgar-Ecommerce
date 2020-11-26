b2e723d6216eedecd976e6a11205deaaf1caa09c
Isaac R <isaacreeder5@gmail.com>
2020-11-25 20:47:36 -0800

    successful multer file upload and render

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ const port = 8000;
 const app = express();
 const db_name = "YoMama_db";
 
+app.get("/", (req, res) => res.json({ msg: "app running" }));
+
+app.use(express.static("uploads"));
+app.use("/api/images", require("./routes/api/images.js"));
 app.use(cors());
 app.use(express.json());
 
